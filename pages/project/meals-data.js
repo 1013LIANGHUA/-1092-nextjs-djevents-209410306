87a31e498b672e3afd4ebbc265e38c5357af9cf7
1234 <209410306@gms.tku.edu.tw>
2021-06-09 07:42:28 +0800

    新增了搜尋的功能

--- pages/project/meals-data.js
+++ pages/project/meals-data.js
@@ -1,28 +1,53 @@
-const data =[   
-    {
-    "image":"https://www.themealdb.com/images/media/meals/1529446137.jpg ",
-    "mealld": 52955,    
-    "h3":"Egg Drop Soup"          
-    },
-    {
-    "image":"https://www.themealdb.com/images/media/meals/2dsltq1560461468.jpg" ,
-    "mealld":52975,
-    "h3":"Tuna and Egg Briks"   
-    },
-    {
-    "image":"https://www.themealdb.com/images/media/meals/1550440197.jpg" ,
-    "mealld":52962,
-    "h3":"Salmon Eggs Eggs Benedict"                       
-    },
-    {
-    "image":"https://www.themealdb.com/images/media/meals/ysqrus1487425681.jpg",
-    "mealld":52816,
-    "h3":"Roasted Eggplant With Tahini, Pine Nuts, and Lentils"    
-    },
-    {
-    "image":"https://www.themealdb.com/images/media/meals/yqwtvu1487426027.jpg",
-    "mealld":52817,
-    "h3":"Stovetop Eggplant With Harissa, Chickpeas, and Cumin Yogurt"     
-    }
-]    
-    export default data;
+const data = [
+  {
+    image: 'https://www.themealdb.com/images/media/meals/1529446137.jpg ',
+    mealld: 52955,
+    title: 'Egg Drop Soup',
+  },
+  {
+    image: 'https://www.themealdb.com/images/media/meals/2dsltq1560461468.jpg',
+    mealld: 52975,
+    title: 'Tuna and Egg Briks',
+  },
+  {
+    image: 'https://www.themealdb.com/images/media/meals/1550440197.jpg',
+    mealld: 52962,
+    title: 'Salmon Eggs Eggs Benedict',
+  },
+  {
+    image: 'https://www.themealdb.com/images/media/meals/ysqrus1487425681.jpg',
+    mealld: 52816,
+    title: 'Roasted Eggplant With Tahini, Pine Nuts, and Lentils',
+  },
+  {
+    image: 'https://www.themealdb.com/images/media/meals/yqwtvu1487426027.jpg',
+    mealld: 52817,
+    h3: 'Stovetop Eggplant With Harissa, Chickpeas, and Cumin Yogurt',
+  },
+  {
+    image: 'https://www.themealdb.com/images/media/meals/1529446137.jpg ',
+    mealld: 52955,
+    title: 'Egg Drop Soup',
+  },
+  {
+    image: 'https://www.themealdb.com/images/media/meals/2dsltq1560461468.jpg',
+    mealld: 52975,
+    title: 'Tuna and Egg Briks',
+  },
+  {
+    image: 'https://www.themealdb.com/images/media/meals/1550440197.jpg',
+    mealld: 52962,
+    title: 'Salmon Eggs Eggs Benedict',
+  },
+  {
+    image: 'https://www.themealdb.com/images/media/meals/ysqrus1487425681.jpg',
+    mealld: 52816,
+    title: 'Roasted Eggplant With Tahini, Pine Nuts, and Lentils',
+  },
+  {
+    image: 'https://www.themealdb.com/images/media/meals/yqwtvu1487426027.jpg',
+    mealld: 52817,
+    h3: 'Stovetop Eggplant With Harissa, Chickpeas, and Cumin Yogurt',
+  }
+];
+export default data;
--- pages/project/meals.js
+++ pages/project/meals.js
@@ -1,8 +1,30 @@
 import styles from '../../styles/meal.module.css'
-import meals from './meals-data';
+//import meals from './meals-data';
 import Head from 'next/head';
+import { useState } from 'react'
 
-export default function meal() {
+export default function meals() {
+   const [input, setInput] = useState('')
+   const [meals, setMeal] = useState([])
+
+   const handleInput = (e) => {
+        setInput(e.target.value)
+   }
+
+
+   console.log(input)
+
+   const handleSubmit = (e) => {
+      e.preventDefault()
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
+      .then(res => res.json())
+      .catch(error => console.error('Error',error))
+      .then(res => {
+        console.log(res);
+        setMeal(res.meals);
+      })
+   }
+
   return (
     <div>
  <Head>
@@ -13,8 +35,8 @@ export default function meal() {
  <h1 className={styles.title__main}>Meal Finder</h1>
  <div className={styles['flex-box']}>
  <form id="submit" className={styles.flex_box}>
- <input type="text" id="search" placeholder="Search for meals or keywords" className={styles.input}/>
- <button className={styles.btn__search} type="submit">
+ <input type="text" value={input} placeholder="Search for meals or keywords" className={styles.input} onChange={handleInput}/>
+ <button className={styles.btn__search} type="submit" onClick={handleSubmit}>
  {/* <FontAwesomeIcon icon={faSearch} /> */}
  <i className="fas fa-search"></i>
  </button>
@@ -24,10 +46,10 @@ export default function meal() {
  <i className='fas fa-random'></i>
  </button>
  </div>
- <div id="result_heading" className={styles.title__search}><h1>Search Results for ' beef ' :</h1></div>
+ <div id="result_heading" className={styles.title__search}><h1>Search Results for ' {input} ' :</h1></div>
  <div id="meals" className={styles.gallery}>
  {meals.map((meals) => (
- <Meal key={meals.id} title={meals.h3} image={meals.image}/>
+ <Meal key={meals.idMeal} title={meals.strMeal} image={meals.strMealThumb}/>
  ))}
  </div>
  </div>
